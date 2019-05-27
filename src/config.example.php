f72636918c7fc43f76279c4b905c5d40c66c25cf
WEBER Antoine <perso@weberantoine.fr>
2019-05-27 13:45:41 +0200

    Create config.example.php

---
+++ src/config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+//Reglages de Spotify API
+$CLIENT_ID = "67xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx0";
+$CLIENT_SECRET = "9xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxb";
+$REDIRECT_URI = "http://your-url-for/spotify-add";
+$ACCESS_TOKEN = "BQCrrrrruURxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx9WyH4KiTyDY9QubWxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxc8NBhvmeSHJ";
+$REFRESH_TOKEN = 'AQDee_6QLRs';
+
+//Reglages de Lametric API
+$LAMETRIC_PUSHURL = 'https://developer.lametric.com/api/v1/dev/widget/update/com.lametric.xxxxxxxxxxxxxxxxxxxxxx/1';
+$LAMETRIC_TOKEN = 'NDkxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=';
